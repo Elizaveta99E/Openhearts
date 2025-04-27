b6f716c3b058782fc073e350c2280a96533ea0b0
Elizaveta99E <elizavetashaman.24@gmail.com>
2025-04-28 02:49:43 +0800

    Try bd

---
+++ DB/controller/staff.controller.js
@@ -0,0 +1,35 @@
+const db = require('../db')
+class StaffController{
+    async createStaff(req,res){
+        const {Mail, Name, Phone, Regdate, Birthday, StaffRole, Photo}=req.body
+        const newSfaff = await db.query(`INSERT INTO Staff (Mail, Name, Phone, Regdate, Birthday, StaffRole, Photo) 
+            values ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, 
+            [Mail, Name, Phone, Regdate, Birthday, StaffRole, Photo])
+        res.json(newSfaff.rows[0])
+    }
+
+    async getStaff(req,res){
+        const users = await db.query('SELECT * FROM Staff')
+        res.json(users.rows)
+    }
+
+    async getOneStaff(req,res){
+        const Id = req.params.id
+        const user = await db.query('SELECT * FROM Staff where Id = $1',[Id])
+        res.json(user.rows[0])
+    }
+
+    async updateStaff(req,res){
+        const {Id, Mail, Name, Phone, Regdate, Birthday, StaffRole, Photo} = req.body
+        const user = await db.query('UPDATE Staff set Mail = $1 Name = $2, Phone = $3, Regdate = $4, Birthday = $5, StaffRole = $6, Photo = $7 where Id = $8 RETURNING *', 
+            [Mail, Name, Phone, Regdate, Birthday, StaffRole, Photo, Id])
+        res.json(user.rows[0])
+    }
+    
+    async deleteStaff(req,res){
+        const Id = req.params.id
+        const user = await db.query('DELETE * FROM Staff where Id = $1',[Id])
+        res.json(user.rows[0])
+    }
+}
+module.exports = new StaffController()

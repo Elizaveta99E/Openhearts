7824e5601f8f8598547c60481c5b0040e169e5ce
Elizaveta99E <elizavetashaman.24@gmail.com>
2025-05-29 13:35:09 +0800

    РАБОТАЕТ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- adminside/vite.config.ts
+++ adminside/vite.config.ts
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',  // Разрешить доступ снаружи контейнера
+    port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8080', // Для локальной разработки
+        target: process.env.VITE_API_URL || 'http://db:8080', // Для локальной разработки
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
